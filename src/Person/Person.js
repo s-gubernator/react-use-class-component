deccce6707a5ed13aafb0f5c029e235423c315bb
s-gubernator <svitlana.gubernator@yahoo.com>
2021-02-22 20:47:52 +0200

    Use children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const person = (props) => {
   return (
-    <p>Ny name is {props.name}</p>
+    <div>
+      <p>I'm { props.name } and I'm { props.age } years old.</p>
+      <p>{ props.children }</p>
+    </div>
   )
 }
 
