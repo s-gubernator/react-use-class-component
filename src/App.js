d0c476aab112a417d7b6c8259b1d1a08eeb12b8e
s-gubernator <svitlana.gubernator@yahoo.com>
2021-02-22 16:10:24 +0200

    Create a functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi there!</h1>
+        <Person />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Edited content'));
