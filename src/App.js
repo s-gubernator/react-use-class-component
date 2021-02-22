2d0a2872106f5af70ec2e0c1ceb7d1f5b44bd38e
s-gubernator <svitlana.gubernator@yahoo.com>
2021-02-22 22:34:39 +0200

    Use state for class component

--- src/App.js
+++ src/App.js
@@ -8,16 +8,25 @@ import Person from './Person/Person.js';
 // const testElement = <p>Some text here</p>
 // ReactDOM.render(testElement, document.getElementById('root'));
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Adam', age: 28 }, 
+      { name: 'Ben', age: 30 }, 
+      { name: 'Chris', age: 27 }
+    ]
+  }
+  
   render() {
     // this isn't html! this is js!!!
     return (
       <div className="App">
         <h1>Hi there!</h1>
-        <Person name='Adam' age='28' />
-        <Person name='Ben'age='30'>
+        <button>Switch name</button>
+        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
+        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>
           My hobbies: Racing
         </Person>
-        <Person name='Chris' age='27'/>
+        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Edited content'));
