84861e9641392d4a54768178635b9d4eba2e9e14
Svitlana Gubernator <svitlana.gubernator@yahoo.com>
2021-02-22 17:48:52 +0200

    Reuse component (#5)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
+// const testElement = <p>Some text here</p>
+// ReactDOM.render(testElement, document.getElementById('root'));
 class App extends Component {
   render() {
     // this isn't html! this is js!!!
     return (
       <div className="App">
         <h1>Hi there!</h1>
-        <Person />
+        <Person  name='Adam' />
+        <Person  name='Ben' />
+        <Person  name='Chris' />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Edited content'));
