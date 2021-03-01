801c162b32b02078f4286085ba5afd443a194b0b
s-gubernator <svitlana.gubernator@yahoo.com>
2021-03-01 20:37:55 +0200

    Use useState hook for functional component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,51 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
 // const testElement = <p>Some text here</p>
 // ReactDOM.render(testElement, document.getElementById('root'));
-class App extends Component {
-  state = {
+
+const App = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Adam', age: 28 }, 
       { name: 'Ben', age: 30 }, 
       { name: 'Chris', age: 27 }
-    ],
-    otherState: 'some other value'
-  }
+    ]
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('some other value');
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
    // console.log('Was clicked');
    // console.log(this);
    // DON'T DO THIS: this.state.person[0].name = 'Max';
-    this.setState( {
+   setPersonsState( {
       persons: [
         { name: 'Max', age: 28 }, 
         { name: 'Ben', age: 30 }, 
         { name: 'Chris', age: 25 }
       ]
      } );
-  }
-  
-// For methods in React, the 'this' keyword should represent the component that owns the method.
-// That is why you should use arrow functions.
-// With arrow functions, 'this' will always represent the object that defined the arrow function.
- 
-  render() {
-    // this isn't html! this is js!!!
-    return (
-      <div className="App">
-        <h1>Hi there!</h1>
-        {/* React event handlers are written inside curly braces and without parentheses:
-          onClick={shoot}  instead of  onClick="shoot()" */}
-        <button onClick={ this.switchNameHandler }>Switch name</button>
-        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
-        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>
-          My hobbies: Racing
-        </Person>
-        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
-      </div>
-    );
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Edited content'));
-  }
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi there!</h1>
+      {/* React event handlers are written inside curly braces and without parentheses:
+        onClick={shoot}  instead of  onClick="shoot()" */}
+      <button onClick={ switchNameHandler }>Switch name</button>
+      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age } />
+      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }>
+        My hobbies: Racing
+      </Person>
+      <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age } />
+    </div>
+  );
 }
 
 export default App;
